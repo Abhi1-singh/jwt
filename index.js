99875d55f8322290719728e7e4e2140d3bc399e4
Abhi1-singh <abhisheksingh909844@gmail.com>
2025-07-01 21:05:24 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,55 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const cors = require("cors");
+const User = require("./models/User");
+require("dotenv").config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log("MongoDB connected"))
+    .catch(err => console.log(err));
+
+// Signup route
+app.post("/signup", async (req, res) => {
+    const { username, password } = req.body;
+    const user = new User({ username, password });
+    await user.save();
+    res.send({ message: "User registered" });
+});
+
+// Login route
+app.post("/login", async (req, res) => {
+    const { username, password } = req.body;
+    const user = await User.findOne({ username, password });
+    if (!user) return res.status(401).json({ error: "Invalid credentials" });
+
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
+    res.send({ token });
+});
+
+// Protected route
+app.get("/profile", verifyToken, (req, res) => {
+    res.send({ message: "Welcome to protected profile route" });
+});
+
+// JWT middleware
+function verifyToken(req, res, next) {
+    const bearer = req.headers.authorization;
+    if (!bearer) return res.status(403).send("Token missing");
+    const token = bearer.split(" ")[1];
+
+    try {
+        const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        req.user = decoded;
+        next();
+    } catch (err) {
+        res.status(403).send("Invalid token");
+    }
+}
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log("Server running on", PORT));
